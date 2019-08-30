4289629ed25189f8604bc766ff8750b3cf875176
kirill.melnikov <kirill.melnikov@avigilon.com>
2019-08-30 11:49:40 -0700

    added changes to listview for style

--- ListView/AddressBookViewModel.cs
+++ ListView/AddressBookViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ListView
 {
@@ -11,25 +12,44 @@ namespace ListView
 	public class AddressBookViewModel
 	{
 		private readonly IAddressFactory m_addressFactory;
+		private string m_number;
+		private string m_name;
+		private string m_address;
+
 		public AddressBookViewModel()
 		{
 			AddressBooks = new ObservableCollection<IAddressBook>();
 			m_addressFactory = new AddressFactory();
 
-			for (int i = 0; i >= 0 && i < 5; i++)
-			{
-				Initialize_();
-			}
+		}
 
-			AddressBooks.Add(new AddressBookOne("Kirill", "43124", "4343"));
+
+		public string Name
+		{
+			get { return m_name; }
+			set { m_name = value; }
+		}
+
+		public string Number
+		{
+			get { return m_number; }
+			set { m_number = value; }
+		}
+
+		public string Address
+		{
+			get { return m_address; }
+			set { m_address = value; }
 		}
 
 		public ObservableCollection<IAddressBook> AddressBooks { get; set; }
-	
-		private void Initialize_()
+
+		public ICommand AddAddressCommand => new RelayCommand(param => AddAddressInfo_());
+
+		private void AddAddressInfo_()
 		{
-			AddressBooks.Add(m_addressFactory.Create("John", "543 6L8", "604-768-1995"));
-		
+			AddressBooks.Add(m_addressFactory.Create(m_name, m_address, m_number));
+
 		}
 
 
